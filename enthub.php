281df7983c31c8f895c1193da5e471c546e74827
LaurencePy <la.eaton@gmail.com>
2025-04-11 14:50:59 +0100

    main page

---
+++ enthub.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ENT Care Hub</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="aos.css">
+    <script defer src="app.js"></script>
+  </head>
+
+  <body>
+  
+
+  <nav class="navbar bg-light px-4">
+  <span class="navbar-brand mb-0 h1">
+    <img src="ent care hub logo.png" alt="logo" width="60" class="me-2" />
+    <span class = "ent-care-hub-text">ENT Care Hub</span>
+  </span>
+  <button class="btn btn-primary ms-auto">Search Consultants</button>
+  </nav>
+
+  <div class="container">
+  <div class="row one">
+    <div class="col-sm-12 col-md-6 about-us">
+      <div class="box about-us">
+        <div class="box-title">About Us</div>
+        <div class="box-description">
+          ENT Care Hub is a leading private healthcare provider in the East Midlands, specialising in ENT (Ear, Nose,
+          and Throat) health services. With a diverse team of highly skilled consultants across multiple specialties, ENT
+          Care Hub aims to provide high-quality healthcare through its network of clinics.
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-12 col-md-6">
+      <div data-aos="zoom-in" data-aos-offset="200" data-aos-duration="800">
+        <img src="doctor.png" class="img-fluid doctor" alt="Doctor image">
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="row two">
+    <div class="col-sm-12 col-md-6 how-to">
+      <div class="box how-to">
+        <div class="box-title">How to Use ENT Care Hub</div>
+          <div class="box-description">
+          Please press any “Search Consultants” button on this page.
+          You will then be taken to our search page in order to filter for, and find, the perfect consultant for your needs.
+          </div>
+      </div>
+  </div>
+
+
+
+</div>
+
+    
+
+  </div>
+  <script src="aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+  </body>
+</html>
