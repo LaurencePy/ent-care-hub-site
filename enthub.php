d1a6cbe1f54104c1a52582b85b1797684c5a2fcf
LaurencePy <la.eaton@gmail.com>
2025-04-14 18:10:05 +0100

    home page map

--- enthub.php
+++ enthub.php
@@ -7,6 +7,12 @@
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="aos.css">
     <script defer src="app.js"></script>
@@ -51,9 +57,40 @@
 
   <div data-aos="fade-up" data-aos-duration="800">
   <div class="row two">
+    
+  <div class="col-sm-12 col-md-6 find-us">
+    <div class = "box locations"id="locations-box">
+    <div class="box-title" id="locations-title"><h2> Our Locations</h2></div>
+    </div>
+    <div id="map">
+    <script>
+    var map = L.map('map').setView([52.7650253, -1.2320904], 9);
+
+    // map set to view from Loughborough University starting point showing all clinics
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  var Riverside_marker = L.marker([52.6369, -1.1398]).addTo(map).bindPopup("Riverside ENT Clinic");
+  var Oakwood_marker = L.marker([52.5200, -1.1700]).addTo(map).bindPopup("Oakwood ENT Centre");
+  var Elmwood_marker = L.marker([52.7400, -1.2100]).addTo(map).bindPopup("Elmwood Medical Hub");
+  var Haven_marker = L.marker([52.9225, -1.4746]).addTo(map).bindPopup("Haven ENT Clinic");
+  var Meadowlands_marker = L.marker([52.9500, -1.4835]).addTo(map).bindPopup("Meadowlands Health Point");
+  var Hillside_marker = L.marker([52.9548, -1.1581]).addTo(map).bindPopup("Hillside ENT Centre");
+  var Valley_marker = L.marker([52.9830, -1.2148]).addTo(map).bindPopup("Valley View Clinic");
+  var Lakeside_marker = L.marker([52.9340, -1.1426]).addTo(map).bindPopup("Lakeside ENT Clinic");
+
+
+
+
+
+  </script>
+    </div>
+  </div>
+
     <div class="col-sm-12 col-md-6 how-to">
       <div class="box how-to">
-        <div class="box-title">How to Use ENT Care Hub</div>
+        <div class="box-title" id ="how-to-use">How to Use ENT Care Hub</div>
           <div class="box-description">
           Please press any “Search Consultants” button on this page.
           You will then be taken to our search page in order to filter for, and find, the perfect consultant for your needs.
@@ -67,6 +104,8 @@
     </div>
   </div>
   </div>
+
+
 </div>
 </div>
 
@@ -94,12 +133,9 @@
     <div data-aos="fade-right" data-aos-duration="800">
   <div class="box jeremy mt-4">
     <div class="d-flex align-items-start">
-      <!-- Avatar on the left -->
       <div class="me-3">
         <img src="avatar_man.png" class="img-fluid man" alt="avatar man" width="150">
       </div>
-
-      <!-- Name and stars on the right -->
       <div>
         <div class="box-title mb-2">Jeremy ⭐⭐⭐⭐⭐</div>
         <div class="box-description">
---
+++ readme.md
@@ -0,0 +1,5 @@
+server issues debugging:
+
+ENSURE mysqli extension is enabled in php.ini
+and
+ext path is set correctly
